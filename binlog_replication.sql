09210a448c6a3fda14b01038d72d0be4535edf9a
gerry <gerasimoskounadis@gmail.com>
2022-10-16 20:01:27 +0300

    replication py

--- binlog_replication.sql
+++ binlog_replication.sql
@@ -1,3 +1,21 @@
+
+CREATE TABLE Orders(
+    OrderId int,
+    OrderStatus varchar(30),
+    LastUpdated timestamp
+);
+
+INSERT INTO Orders
+    VALUES(1, 'Backordered', '2020-06-01 12:00:00');
+INSERT INTO Orders
+    VALUES(1, 'Shipped', '2020-06-09 12:00:25');
+INSERT INTO Orders
+    VALUES(2, 'Shipped', '2020-07-11 3:05:00');
+INSERT INTO Orders
+    VALUES(1,'Shipped', '2020-06-09 11:50:00');
+INSERT INTO Orders
+    VALUES(3,'Shipped', '2020-07-12 12:00:00');
+
 SELECT variable_value as bin_log_status
 FROM performance_schema.global_variables
 WHERE variable_name='log_bin';
